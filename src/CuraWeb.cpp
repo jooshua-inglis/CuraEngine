94b9b787d7cf0daac811a31aaa65a7d14db8a086
Joshua Inglis <jt.inglis@outlook.com>
2021-11-21 09:50:33 +1000

    .

--- src/CuraWeb.cpp
+++ src/CuraWeb.cpp
@@ -11,7 +11,6 @@ extern "C"
 	int slice_js(char *input_stl, char *config_json, char *gcode_output_file)
 	{
 		char *args[9] = {"cura", "slice", "-v", "-j", config_json, "-o", gcode_output_file, "-l", input_stl};
-		printf("MyFunction Called\n");
 		cura::Application::getInstance().run(6, args);
 		return 0;
 	}
